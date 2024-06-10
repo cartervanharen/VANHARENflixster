b444daf5c59621a7008411bbcae882f130c80039
Carter Min-Kee Vanharen <cartervanharen@meta.com>
2024-06-10 14:07:37 -0700

    added api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const App = () => {
 
   return(
+
   <div className="App">
     <p>test</p>
+    {console.log(apiKey)}
   </div>
-
   )
+
+
+  
 }
 
 export default App
